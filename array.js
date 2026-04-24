e8d9d82b4d92fcf55660867506cd476562c6f269
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2026-04-24 19:08:59 +0300

    first commit

---
+++ array.js
@@ -0,0 +1,14 @@
+const arr = [1, 2, 3, 4]
+
+const forEach = arr.forEach((el, i) => {
+    return el*2;
+})
+
+
+const map = arr.map((el, i) => {
+    return el*2;
+})
+
+
+console.log(forEach);
+console.log(map);
